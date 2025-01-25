05ee6a0215dbe5380aebb9ef1fae66e9e18eb232
Dan Shaw <dcshaw.en@gmail.com>
2025-01-25 15:38:36 -0400

    Delete flask_auth_app directory

--- flask_auth_app/SQLusrPswHsh.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Users (
-	id INT IDENTITY (1,1),
-	username NVARCHAR (50) UNIQUE NOT NULL,
-	password_hash NVARCHAR (255) NOT NULL,
-	CONSTRAINT PK_Users PRIMARY KEY (id)
-);
-
-Select * from Users;
